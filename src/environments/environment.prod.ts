47f4bc5d9c006993d7381358592431ffcda61f8c
Ondrej Oravčok <Ondrej.Oravcok@cloudflight.io>
2021-05-27 17:24:12 +0200

    enable live update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  production: true,
+  superFaktura: {
+    url: 'http://localhost:4200/mojafaktura',
+    authHeader: '',
+  },
+  wooCommerce: {
+    url: 'http://localhost:4200/bvx-eshop',
+    apiKey: '',
+    secret: '',
+  },
+  skPost: {
+    url: 'http://localhost:4200/sk-post-api',
+    userId: '',
+    apiKey: '',
+  },
+  packeta: {
+    url: 'http://localhost:4200/packeta',
+    apiPassword: '',
+  },
 };
--- start-bvx-orders.bat
+++ start-bvx-orders.bat
@@ -1,5 +1,11 @@
 @echo off
 cd /D "%~dp0"
+ECHO Checking if there is newer version:
+git pull
+
+ECHO Installing dependencies (maybe needed):
+call npm i
+
 ECHO Starting bvx-orders local server
 npm run start
 PAUSE
